679dc1050f99c140062605732dec6b9c609ebcc5
DavidLucasK <davidlucasfr70@gmail.com>
2024-09-16 14:26:52 -0300

    teste

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: ['https://pontogorillaback.vercel.app', 'https://resetpassgorilla.vercel.app/', 'http://127.0.0.1:5500',]
+    origin: ['https://pontogorillaback.vercel.app', 'https://resetpassgorilla.vercel.app', 'http://127.0.0.1:5500',]
 }));
 app.use(express.json());
 
