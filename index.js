581b514329a45222834fad0ed97ad8ac7c296014
DavidLucasK <davidlucasfr70@gmail.com>
2024-09-16 16:39:42 -0300

    Adding image gorilla logo

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
     origin: ['https://pontogorillaback.vercel.app', 'https://resetpassgorilla.vercel.app', 'http://127.0.0.1:5500',]
