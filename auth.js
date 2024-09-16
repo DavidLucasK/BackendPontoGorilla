f18917b3bfd4f9e4bf663577ad692129dc635673
DavidLucasK <davidlucasfr70@gmail.com>
2024-09-16 10:29:47 -0300

    first commit to backend app ponto gorilla

---
+++ auth.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const { createClient } = require('@supabase/supabase-js');
+const multer = require('multer');
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({ 
+    storage: storage, 
+    limits: { fileSize: 10 * 1024 * 1024 } // Limite de 10MB
+});
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Configura o transporte de e-mail
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
+
+// Endpoint para registro
+router.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const { data: existingUser, error: existingUserError } = await supabase
+            .from('users')
+            .select('*')
+            .eq('email', email)
+            .single();
+
+        if (existingUserError && existingUserError.code !== 'PGRST116') {
+            throw existingUserError;
+        }
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'Usuário já existe' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const { data, error } = await supabase
+            .from('users')
+            .insert([{ email, password: hashedPassword }]);
+
+        if (error) {
+            throw error;
+        }
+
+        res.status(201).json({ message: 'Conta criada com sucesso!' });
+    } catch (err) {
+        console.error('Erro ao registrar o usuário:', err);
+        res.status(500).json({ message: 'Erro no servidor' });
+    }
+});
+
+// Endpoint para login
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const { data: user, error } = await supabase
+            .from('users')
+            .select('*')
+            .eq('email', email)
+            .single();
+
+        if (error && error.code === 'PGRST116') {
+            return res.status(400).json({ message: `Senha incorreta para ${email} ou email não cadastrado` });
+        } else if (error) {
+            throw error;
+        }
+
+        if (!user) {
+            return res.status(400).json({ message: `Usuário ${user} não existe!` });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: `Senha incorreta.` });
+        }
+
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.json({ token, userId: user.id, message: 'Login bem-sucedido!' });
+    } catch (err) {
+        console.error('Erro ao fazer login:', err);
+        res.status(500).json({ message: 'Erro no servidor' });
+    }
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: ['https://pontogorillaback.vercel.app', 'http://127.0.0.1:5500']
+}));
+app.use(express.json());
+
+const authRoutes = require('./auth');
+app.use('/api/auth', authRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
